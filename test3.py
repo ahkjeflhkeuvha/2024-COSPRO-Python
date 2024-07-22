80f1e9a0afb4974ebbcd45d34ae4da4a1b77266d
jieun <w2317@e-mirim.hs.kr>
2024-07-22 11:08:57 +0900

    test 4

--- test3.py
+++ test3.py
@@ -1,4 +1,4 @@
 for i in range(2, 9 + 1):
     print("----- {}단 -----".format(i))
     for j in range(1, 9 + 1):
-        print("{} * {} = {}".format(i, j, i*j))
+        print("{} X {} = {}".format(i, j, i*j))
---
+++ test4.py
@@ -0,0 +1,4 @@
+for i in range(2, 8 + 1, 2):
+    print(f"----- {i}단 -----")
+    for j in range(1, 9 + 1):
+        print(f"{i} X {j} = {i*j}")
